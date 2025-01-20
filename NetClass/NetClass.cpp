45d5fe031dca43a3f819fa09e6135a06ec500665
kgca44_4 <seojunes@naver.com>
2025-01-20 14:57:19 +0900

    서버구축의 가장 기초.

--- NetClass/NetClass.cpp
+++ NetClass/NetClass.cpp
@@ -1,43 +1,78 @@
-﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS                 //이전 API함수에서 발생하는 컴파일 경고를 무시하기 위한 매크로
 
 // 네트워크를 사용하기 위한 환경설정.
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <mswsock.h>
-#include <iostream>
+#include <winsock2.h>           //Windows 소켓관련 함수 및 구조체 정의
+#include <ws2tcpip.h>           //최신 TCP/IP 관련 기능.
+#include <mswsock.h>            //Microsoft 확장 소켓 기능.
+#include <iostream>             
+#include <string>               
 
-#pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "mswsock.lib")
+#pragma comment(lib, "ws2_32.lib")          //네트워크 함수 사용을 위해 ws2_32.lib라이브러리 링크
+#pragma comment(lib, "mswsock.lib")         //고급 소켓 기능을 위한 라이브러리 링크.
 
 
 int main()
 {
-    // 윈속 초기화( 버전 선택)
-    WSADATA wsa;
-    int iRet = WSAStartup(MAKEWORD(2, 2), &wsa); // 2.2버젼
-    if (iRet != 0) return 1;
+    // 윈속 초기화( 버전 선택) WSAStartup
+    WSADATA wsa;                //윈속 정보를 담는 구조체
+    int iRet = WSAStartup(MAKEWORD(2, 2), &wsa); // WinSock 2.2버젼 버젼 초기화
+    if (iRet != 0) return 1;                    
     // 윈도우에서는 0값이 떨어지면 오류라고 보면된다.
     // 윈속을 만들면 이제 서버가 사용가능하고, 2.2버젼 위의 버전이 없다.
+    // Windows 환겨엥서는 반드시 소켓 사용 전에 WSAStartup을 호출해야함.
+
 
     // 소켓생성 : 네크워크를 할때 연결을 하는 도구. 서버 <-> 클라
-    SOCKET sock;
+    SOCKET sock;            // 소켓을 저장할 변수 sock.
     // TCP & IP 프로토콜 
     sock = socket(AF_INET, SOCK_STREAM, 0);  //원래는 0자리에 IPPROTO_TCP를 넘겨주는게 FM이다. AF_INET이 IP이다.
-    //전화번호 만들기
-    SOCKADDR_IN sa; //연결해주는 전화번호 (구조체로 되어있음)
-    ZeroMemory(&sa, sizeof(sa));
+                //AF_INET: Ipv4주소체계사용, SOCK_STREAM: TCP 연결 방식 사용  
+    
+                
+    //전화번호 만들기(서버 주소 설정)
+    SOCKADDR_IN sa; //연결해주는 전화번호 (구조체로 되어있음) << 서버 주소 정보를 저장할 구조체
+    ZeroMemory(&sa, sizeof(sa));                              //메모리 초기화
     // 3가지를 설정해주면 해당 전화번호로 전화를 해준다.
     sa.sin_family = AF_INET;  //ip프로토콜
-    sa.sin_addr.s_addr = inet_addr("192.168.0.88");  // 주소                    << 전화번호
+    sa.sin_addr.s_addr = inet_addr("192.168.0.44");  // 주소                    << 전화번호   inet_addr 비추천 대신에 inet_pton 권장.
     sa.sin_port = htons(10000);         //번호생성 완료. 응용프로그램 인덱스.    << 전화번호로 생각하면 받는사람
+    // 포트를 네트워크 바이트 순서(빅 엔디안)로 변환.
 
-    //접속 명령어
+    //접속 명령어( 서버 접속 )
     iRet = connect(sock, (SOCKADDR*)&sa, sizeof(sa));
     //전화를 안받으면 에러를 체크한다.
+    //connect를 통해서 서버 접속을 시도해본다. sa라는 서버 주소관련 변수를 통해서 접근 시도.
     if (iRet == SOCKET_ERROR)       return 1;
-    char buffer[256] = "서준입니다.";//{ 0, };
-    //send가 얼마만큼 보내줬는지를 반환해준다.
-    int iSendSize = send(sock, buffer, sizeof(buffer), 0);
+    //char buffer[256] = "서준입니다.";//{ 0, };
+    ////send가 얼마만큼 보내줬는지를 반환해준다.
+    //int iSendSize = send(sock, buffer, sizeof(buffer), 0);
+    
+    
+    std::string SendBuf;
+    SendBuf.resize(256);
+    std::string RecvBuf;
+
+    while (1)
+    {
+        std::getline(std::cin, SendBuf);
+        if (SendBuf.empty())
+        {
+            break;
+        }
+        int iSendSize = send(sock, &SendBuf[0], SendBuf.size(), 0);
+        if (iSendSize == 0 || iSendSize == SOCKET_ERROR) {
+            std::cout << "서버가 종료되었거나 문제가 발생" << std::endl;
+            break;
+        }
+        RecvBuf.resize(256);
+        int iRecvSize = recv(sock, &RecvBuf[0], RecvBuf.size(), 0);
+        if (iRecvSize == 0 || iRecvSize == SOCKET_ERROR) {
+            std::cout << "서버가 종료되었거나 문제가 발생" << std::endl;
+            break;
+        }
+        std::cout << RecvBuf << std::endl;
+        RecvBuf.clear();
+    }
 
     closesocket(sock); // 소켓을 닫아준다.
 
--- NetworkServer/NetworkServer.cpp
+++ NetworkServer/NetworkServer.cpp
@@ -1,44 +1,78 @@
 ﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
-// 네트워크를 사용하기 위한 환경설정.
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <mswsock.h>
 #include <iostream>
-
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "mswsock.lib")
-
-
 int main()
 {
+	//윈속 초기화
+	WSADATA wsa;
+	int iRet = WSAStartup(MAKEWORD(2, 2), &wsa);
+	if (iRet != 0) return 1;
 
-    WSADATA wsa;
-    int iRet =  WSAStartup(MAKEWORD(2,2), &wsa);
-    if (iRet != 0) return 1;
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0); 
-    SOCKADDR_IN sa; 
-    ZeroMemory(&sa, sizeof(sa));
-    sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = htonl(INADDR_ANY);//inet_addr("192.168.0.110"); 
-    sa.sin_port = htons(10000);        
-    //iRet = connect(sock, (SOCKADDR*)&sa, sizeof(sa));
-    iRet = bind(sock, (SOCKADDR*)&sa, sizeof(sa));  // 집에 전화를 개통해놓음. 클라는 connect 서버는 bind 
-    if (iRet == SOCKET_ERROR) return 1;
-    listen(sock, SOMAXCONN);
-    if (iRet == SOCKET_ERROR) return 1;
-    SOCKADDR_IN clientaddr;
-    int addlen = sizeof(clientaddr);
-    SOCKET clientSock = accept(sock, (SOCKADDR*)&clientaddr, &addlen);             //누가 전화를 했는지 return까지 해준다. 
-    // 전화벨이 울리면, 누가 전화했는지 확인하는 것이 clientaddr 
-    //accept가 반환이 됐다는 것은 이미 연결이 되어있다는 이야기.
-    // 두개의 소켓이 open인 상태를 연결되었다고 한다.
-    
-    char buffer[256] = "0, ";
-    int iRedvSize = recv(clientSock, buffer, sizeof(buffer), 0);
-    std::cout << buffer << std::endl;
-    closesocket(clientSock);
-    closesocket(sock);
-    WSACleanup();
+	//소켓생성
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);// IPPROTO_TCP);
+
+	//서버 주소 설정 및 바인딩
+	SOCKADDR_IN sa;
+	ZeroMemory(&sa, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);// 전화번호
+	sa.sin_port = htons(10000); // 받는 사람
+
+	//소켓 바인딩(bind)
+	iRet = bind(sock, (SOCKADDR*)&sa, sizeof(sa));          // 소켓을 특정 IP및 포트와 연결
+	if (iRet == SOCKET_ERROR) return 1;
+
+	//연결 대기 설정(listen)
+	iRet = listen(sock, SOMAXCONN);         //클라이언트를 받을 준비
+	if (iRet == SOCKET_ERROR) return 1;
+
+	//클라이언트 연결 수락 (accept) 
+	SOCKADDR_IN clientaddr;
+	int addlen = sizeof(clientaddr);
+
+	//데이터 수신 및 전송 루프
+	std::string SendBuf;
+	while (1)
+	{
+		SOCKET clientSock = accept(sock, (SOCKADDR*)&clientaddr, &addlen);
+		std::cout << "접속(IP):" << inet_ntoa(clientaddr.sin_addr)
+			<< "포트번호" << ntohs(clientaddr.sin_port)
+			<< std::endl;
+		while (1)
+		{
+			char Recvbuffer[256] = { 0, };
+			int iRecvSize = recv(clientSock, Recvbuffer, sizeof(Recvbuffer), 0);
+			if (iRecvSize == 0)
+			{
+				std::cout << "접속종료(IP):" << inet_ntoa(clientaddr.sin_addr)
+					<< "포트번호" << ntohs(clientaddr.sin_port)
+					<< std::endl;
+				closesocket(clientSock);
+			}
+			std::cout << Recvbuffer << std::endl;
+			SendBuf = Recvbuffer;
+			SendBuf += " 이렇게 말했음.";
+			int iSendSize = send(clientSock,
+				&SendBuf[0], SendBuf.size(), 0);
+			if (iSendSize == SOCKET_ERROR)
+			{
+				std::cout << "접속종료(IP):" << inet_ntoa(clientaddr.sin_addr)
+					<< "포트번호" << ntohs(clientaddr.sin_port)
+					<< std::endl;
+				closesocket(clientSock);
+				std::cout << "서버에 문제가 있다." << std::endl;
+				break;
+			}
+			SendBuf.clear();
+		}
+	}
+	// Sleep(10000);
+	// 서버 종료 처리
+	closesocket(sock);
+	WSACleanup();
+
 }
-
