f13b61f776e0d4c67f39a100e9bcb696fdad195d
kgca44_4 <seojunes@naver.com>
2025-01-21 17:08:39 +0900

    서버와 클라의 블록형 함수를 논블록킹으로 변환 그거에 따라서 여러 구조 변경.

--- NetClass/NetClass.cpp
+++ NetClass/NetClass.cpp
@@ -1,83 +1,100 @@
-﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS                 //이전 API함수에서 발생하는 컴파일 경고를 무시하기 위한 매크로
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mswsock.h>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "mswsock.lib")
 
-// 네트워크를 사용하기 위한 환경설정.
-#include <winsock2.h>           //Windows 소켓관련 함수 및 구조체 정의
-#include <ws2tcpip.h>           //최신 TCP/IP 관련 기능.
-#include <mswsock.h>            //Microsoft 확장 소켓 기능.
-#include <iostream>             
-#include <string>               
-
-#pragma comment(lib, "ws2_32.lib")          //네트워크 함수 사용을 위해 ws2_32.lib라이브러리 링크
-#pragma comment(lib, "mswsock.lib")         //고급 소켓 기능을 위한 라이브러리 링크.
-
-
+// 오류 처리 send()및 recv() 호출 결과를 확인.
+bool Check(int iCode)
+{
+    if (iCode == SOCKET_ERROR)
+    {
+        int iError = WSAGetLastError();
+        if (iError != WSAEWOULDBLOCK)           // WSAEVOULDBLOCK : 논 블로킹상태에서 데이터가 준비되지 않았음을 의미하므로 무시.
+        {
+            std::cout << "서버가 비정상종료,문제가 발생" << std::endl;               // 다른 오류가 발생시 프로그램 종료
+            exit(1);
+        }
+        return false;
+    }
+    return true;
+}
 int main()
 {
-    // 윈속 초기화( 버전 선택) WSAStartup
-    WSADATA wsa;                //윈속 정보를 담는 구조체
-    int iRet = WSAStartup(MAKEWORD(2, 2), &wsa); // WinSock 2.2버젼 버젼 초기화
-    if (iRet != 0) return 1;                    
-    // 윈도우에서는 0값이 떨어지면 오류라고 보면된다.
-    // 윈속을 만들면 이제 서버가 사용가능하고, 2.2버젼 위의 버전이 없다.
-    // Windows 환겨엥서는 반드시 소켓 사용 전에 WSAStartup을 호출해야함.
+    // 윈속 초기화( 버전선택)
+    WSADATA wsa;
+    int iRet = WSAStartup(MAKEWORD(2, 2), &wsa);
+    if (iRet != 0) return 1;
+    // 소켓생성
+    SOCKET sock;
+    //  TCP & IP 프로토콜
+    sock = socket(AF_INET, SOCK_STREAM, 0);// IPPROTO_TCP);
 
-
-    // 소켓생성 : 네크워크를 할때 연결을 하는 도구. 서버 <-> 클라
-    SOCKET sock;            // 소켓을 저장할 변수 sock.
-    // TCP & IP 프로토콜 
-    sock = socket(AF_INET, SOCK_STREAM, 0);  //원래는 0자리에 IPPROTO_TCP를 넘겨주는게 FM이다. AF_INET이 IP이다.
-                //AF_INET: Ipv4주소체계사용, SOCK_STREAM: TCP 연결 방식 사용  
-    
-                
-    //전화번호 만들기(서버 주소 설정)
-    SOCKADDR_IN sa; //연결해주는 전화번호 (구조체로 되어있음) << 서버 주소 정보를 저장할 구조체
-    ZeroMemory(&sa, sizeof(sa));                              //메모리 초기화
-    // 3가지를 설정해주면 해당 전화번호로 전화를 해준다.
-    sa.sin_family = AF_INET;  //ip프로토콜
-    sa.sin_addr.s_addr = inet_addr("192.168.0.44");  // 주소                    << 전화번호   inet_addr 비추천 대신에 inet_pton 권장.
-    sa.sin_port = htons(10000);         //번호생성 완료. 응용프로그램 인덱스.    << 전화번호로 생각하면 받는사람
-    // 포트를 네트워크 바이트 순서(빅 엔디안)로 변환.
-
-    //접속 명령어( 서버 접속 )
+    SOCKADDR_IN sa;
+    ZeroMemory(&sa, sizeof(sa));
+    sa.sin_family = AF_INET;
+    //error C4996: 'inet_addr': Use inet_pton() or InetPton() 
+    //instead or define _WINSOCK_DEPRECATED_NO_WARNINGS to disable deprecated API warnings
+    sa.sin_addr.s_addr = inet_addr("192.168.0.88");// 전화번호
+    sa.sin_port = htons(10000); // 받는 사람
     iRet = connect(sock, (SOCKADDR*)&sa, sizeof(sa));
-    //전화를 안받으면 에러를 체크한다.
-    //connect를 통해서 서버 접속을 시도해본다. sa라는 서버 주소관련 변수를 통해서 접근 시도.
-    if (iRet == SOCKET_ERROR)       return 1;
-    //char buffer[256] = "서준입니다.";//{ 0, };
-    ////send가 얼마만큼 보내줬는지를 반환해준다.
-    //int iSendSize = send(sock, buffer, sizeof(buffer), 0);
-    
-    
+    if (iRet == SOCKET_ERROR) return 1;
+
+    // 넌블록킹 소켓전환
+    u_long on = 1;
+    ioctlsocket(sock, FIONBIO, &on);        //ioctlsocket에서 FIONBIO를 통해서 논블록킹으로 전환
+
     std::string SendBuf;
-    SendBuf.resize(256);
+    SendBuf.reserve(256);
     std::string RecvBuf;
 
     while (1)
     {
-        std::getline(std::cin, SendBuf);
-        if (SendBuf.empty())
+        if (_kbhit() == 1)                  // 키입력이 있는지 없는지를 확인
         {
-            break;
-        }
-        int iSendSize = send(sock, &SendBuf[0], SendBuf.size(), 0);
-        if (iSendSize == 0 || iSendSize == SOCKET_ERROR) {
-            std::cout << "서버가 종료되었거나 문제가 발생" << std::endl;
-            break;
+            int iValue = _getche();         // 글자를 받아서 한글자씩 처리 (화면에 보여줌)(안보여주는 함수는 _getch)
+            if (SendBuf.empty())
+            {
+                if (iValue == '\r')         //\r : 엔터키를 입력시 생성
+                {
+                    break;
+                }
+            }
+            SendBuf.push_back(iValue);
+            //std::getline(std::cin, SendBuf);
+           /* if (SendBuf.empty())
+            {
+                break;
+            }*/
+            if (iValue == '\r')
+            {
+                int iSendSize = send(sock, &SendBuf[0], SendBuf.size() - 1, 0);             // 메세지 전송 후 오류 검사 수행
+                if (!Check(iSendSize)) {
+                    break;
+                }
+                SendBuf.clear();
+            }
         }
         RecvBuf.resize(256);
-        int iRecvSize = recv(sock, &RecvBuf[0], RecvBuf.size(), 0);
-        if (iRecvSize == 0 || iRecvSize == SOCKET_ERROR) {
-            std::cout << "서버가 종료되었거나 문제가 발생" << std::endl;
+        int iRecvSize = recv(sock, &RecvBuf[0], RecvBuf.size(), 0);                         //데이터 수신
+        if (iRecvSize == 0)
+        {
+            std::cout << "서버가 정상종료" << std::endl;
             break;
         }
-        std::cout << RecvBuf << std::endl;
+        if (Check(iRecvSize))
+        {
+            std::cout << std::endl;
+            std::cout << RecvBuf << std::endl;
+        }
         RecvBuf.clear();
     }
-
-    closesocket(sock); // 소켓을 닫아준다.
-
+    closesocket(sock);
     // 윈속 소멸
     WSACleanup();
-    // 생성과 초기화를 1번만 넣어주면 된다.
+    std::cout << "Hello World!\n";
 }
-
--- NetworkServer/NetworkServer.cpp
+++ NetworkServer/NetworkServer.cpp
@@ -3,76 +3,163 @@
 #include <ws2tcpip.h>
 #include <mswsock.h>
 #include <iostream>
+#include <list>
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "mswsock.lib")
+
+
+// 클라이언트 정보 저장
+struct THost
+{
+    SOCKET      sock;                           // 클라이언트 소켓 핸들
+    SOCKADDR_IN addr;                           // 클라이언트 주소 및 포트 정보
+    char        Recvbuffer[256] = { 0, };       // 수신 데이터 저장 공간
+    int         iRecvBytes;                     // 수신된 바이트 수 저장.
+    bool        bConnect;                       // 연결여부 (종료 시 false 설정)
+};
+//에러처리
+bool Check(THost& host, int iCode)
+{
+    if (iCode == SOCKET_ERROR)
+    {
+        int iError = WSAGetLastError();
+        if (iError != WSAEWOULDBLOCK)
+        {
+            host.bConnect = false;
+        }
+    }
+    else
+    {
+        return true;
+    }
+    return false;
+}
+bool Check(int iCode)
+{
+    if (iCode == SOCKET_ERROR)
+    {
+        int iError = WSAGetLastError();
+        if (iError != WSAEWOULDBLOCK)
+        {
+            exit(1);
+        }
+    }
+    else
+    {
+        return true;
+    }
+    return false;
+}
 int main()
 {
-	//윈속 초기화
-	WSADATA wsa;
-	int iRet = WSAStartup(MAKEWORD(2, 2), &wsa);
-	if (iRet != 0) return 1;
+    WSADATA wsa;
+    int iRet = WSAStartup(MAKEWORD(2, 2), &wsa);                // 윈속 초기화
+    if (iRet != 0) return 1;
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);// IPPROTO_TCP); TCP소켓 생성
+    SOCKADDR_IN sa;
+    ZeroMemory(&sa, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);// 전화번호
+    sa.sin_port = htons(10000); // 받는 사람    
+    iRet = bind(sock, (SOCKADDR*)&sa, sizeof(sa));              // 서버 소켓을 포트 10000에 바인딩
+    if (iRet == SOCKET_ERROR) return 1;
+    iRet = listen(sock, SOMAXCONN);                             // 클라이언트 연결 대기 상태로 전환.
+    if (iRet == SOCKET_ERROR) return 1;
 
-	//소켓생성
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);// IPPROTO_TCP);
+    // 넌블록킹 소켓전환
+    u_long on = 1;
+    ioctlsocket(sock, FIONBIO, &on);
 
-	//서버 주소 설정 및 바인딩
-	SOCKADDR_IN sa;
-	ZeroMemory(&sa, sizeof(sa));
-	sa.sin_family = AF_INET;
-	sa.sin_addr.s_addr = htonl(INADDR_ANY);// 전화번호
-	sa.sin_port = htons(10000); // 받는 사람
+    SOCKADDR_IN clientaddr;                                     // 클라이언트 연결요청을 수락하고, THost 객체에 저장.
+    int addlen = sizeof(clientaddr);
 
-	//소켓 바인딩(bind)
-	iRet = bind(sock, (SOCKADDR*)&sa, sizeof(sa));          // 소켓을 특정 IP및 포트와 연결
-	if (iRet == SOCKET_ERROR) return 1;
+    std::list<THost> userList;
+    while (userList.size() != 1)
+    {
+        SOCKET clientSock = accept(sock, (SOCKADDR*)&clientaddr,
+            &addlen);
+        if (Check(clientSock))
+        {
+            // 정상작업
+            std::cout << "접속(IP):" << inet_ntoa(clientaddr.sin_addr)
+                << "포트번호" << ntohs(clientaddr.sin_port)
+                << std::endl;
+            THost host;
+            host.addr = clientaddr;
+            host.sock = clientSock;
+            host.bConnect = true;
+            userList.push_back(host);
+        }
+    }
+    
+    while (1)
+    {
+        //데이터 수신 및 처리
+        for (auto iter = userList.begin();
+            iter != userList.end();
+            iter++)
+        {
+            THost& host = *iter;
+            if (host.bConnect == false) continue;
+            ZeroMemory(host.Recvbuffer, sizeof(char) * 256);
+            host.iRecvBytes = 0;
+            int iRecvSize = recv(host.sock,
+                host.Recvbuffer,
+                sizeof(host.Recvbuffer), 0);
+            if (iRecvSize == 0)
+            {
+                host.bConnect = false;
+            }
+            if (Check(host, iRecvSize))
+            {
+                host.iRecvBytes = iRecvSize;
+                std::cout << host.Recvbuffer << std::endl;
+            }
+        }
 
-	//연결 대기 설정(listen)
-	iRet = listen(sock, SOMAXCONN);         //클라이언트를 받을 준비
-	if (iRet == SOCKET_ERROR) return 1;
+        //브로드캐스트 (데이터 전송)
+        for (auto recvHost = userList.begin();
+            recvHost != userList.end();
+            recvHost++)
+        {
+            THost& sendData = *recvHost;
+            if (sendData.bConnect == false) continue;
+            if (sendData.iRecvBytes <= 0)
+            {
+                continue;
+            }
+            for (auto sendHost = userList.begin(); sendHost != userList.end(); sendHost++)
+            {
+                THost& host = *sendHost;
+                if (host.bConnect == false) continue;
+                int iSendSize = send(host.sock, sendData.Recvbuffer,
+                    sendData.iRecvBytes, 0);
+                Check(host, iSendSize);
+            }
+        }
 
-	//클라이언트 연결 수락 (accept) 
-	SOCKADDR_IN clientaddr;
-	int addlen = sizeof(clientaddr);
+        // 종료처리
+        for (auto iter = userList.begin();
+            iter != userList.end(); )
+        {
+            THost& host = *iter;
+            if (host.bConnect == false)
+            {
+                std::cout << "접속종료(IP):" <<
+                    inet_ntoa(host.addr.sin_addr)
+                    << "포트번호" << ntohs(host.addr.sin_port)
+                    << std::endl;
+                closesocket(host.sock);
+                iter = userList.erase(iter);
+            }
+            else
+            {
+                iter++;
+            }
+        }
+    }
 
-	//데이터 수신 및 전송 루프
-	std::string SendBuf;
-	while (1)
-	{
-		SOCKET clientSock = accept(sock, (SOCKADDR*)&clientaddr, &addlen);
-		std::cout << "접속(IP):" << inet_ntoa(clientaddr.sin_addr)
-			<< "포트번호" << ntohs(clientaddr.sin_port)
-			<< std::endl;
-		while (1)
-		{
-			char Recvbuffer[256] = { 0, };
-			int iRecvSize = recv(clientSock, Recvbuffer, sizeof(Recvbuffer), 0);
-			if (iRecvSize == 0)
-			{
-				std::cout << "접속종료(IP):" << inet_ntoa(clientaddr.sin_addr)
-					<< "포트번호" << ntohs(clientaddr.sin_port)
-					<< std::endl;
-				closesocket(clientSock);
-			}
-			std::cout << Recvbuffer << std::endl;
-			SendBuf = Recvbuffer;
-			SendBuf += " 이렇게 말했음.";
-			int iSendSize = send(clientSock,
-				&SendBuf[0], SendBuf.size(), 0);
-			if (iSendSize == SOCKET_ERROR)
-			{
-				std::cout << "접속종료(IP):" << inet_ntoa(clientaddr.sin_addr)
-					<< "포트번호" << ntohs(clientaddr.sin_port)
-					<< std::endl;
-				closesocket(clientSock);
-				std::cout << "서버에 문제가 있다." << std::endl;
-				break;
-			}
-			SendBuf.clear();
-		}
-	}
-	// Sleep(10000);
-	// 서버 종료 처리
-	closesocket(sock);
-	WSACleanup();
+    closesocket(sock);
+    WSACleanup();
 
 }
